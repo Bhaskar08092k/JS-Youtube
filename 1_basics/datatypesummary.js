23f44f7897fd33655c2138ca7104326348ca55d5
Bhaskar08092k <kornanabhaskarrao@gmail.com>
2025-07-30 12:35:22 +0530

    summary of datatypes

---
+++ 1_basics/datatypesummary.js
@@ -0,0 +1,30 @@
+//   Primitive data types
+ 
+//   =>String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+//  let name = "bhaskar";                    String
+//  let age = 25;                            Number
+//  let isMarried = false;                   Boolean
+//  let address = null;                      Null
+//  let phone;                               Undefined
+//  let uniqueId = Symbol("123");            Symbol 
+//  let UniqueId2 = Symbol("123");           Symbol (different from uniqueId)
+// let bigNumber = 12345678901234567890n;    BigInt
+
+
+//   non-primitive data types(reference types)
+
+//   => Object, Array, Function
+  
+//    let names = ["bhaskar", "amith", "praveen", "abhi"];       Array    
+    
+
+//    let bio = {
+//        firstName: "bhaskar",    
+//           lastName: "rao",                                 key-value pairs
+//             age: 25,
+//    }                                                           Object
+           
+//    let hi = function()    { 
+//          console.log("Hello World")
+//    }                                                         Function
