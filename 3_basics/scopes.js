61752bd5966867cc11625e4be9430d6828500d8e
Bhaskar08092k <kornanabhaskarrao@gmail.com>
2025-08-14 12:34:21 +0530

    part 2

--- 3_basics/scopes.js
+++ 3_basics/scopes.js
@@ -1,13 +1,53 @@
 
-const  b =20;
-var c =30;
+// const  b =20;
+// var c =30;
 // if(true){
 //     var c=40;
 // }
 // console.log(c) //40
-let a=10;
-if(true){
-    let a=9;
-    console.log(a);  // 9
-}
-console.log(a) // 10
+
+
+// let a=10;
+// if(true){
+//     let a=9;
+//     console.log(a);  // 9
+// }
+// console.log(a) // 10
+
+// function one(){
+//     const name ='bhaskar' ;
+
+//     function two(){
+//         let age = 25; 
+//       console.log(name)
+//     }
+//    // console.log(age); // not excutes due to funtion scope
+//     two()
+// }
+// one()
+
+
+
+// if(true){
+//     let username = 'bhaskar'
+//     if(username==='bhaskar'){
+//         let age=25
+//         console.log(username + age)
+//     }
+//     //console.log(age)
+// }
+// //console.log(username)
+
+
+
+
+// ************************ intresting ********************************//
+// console.log(addone(5)) // 6
+// function addone(num){
+//  return num+1
+// }
+
+// //addtwo(5)  // ReferenceError: Cannot access 'addtwo' before initialization
+// let addtwo = function(num){
+//     return num+2
+// }
